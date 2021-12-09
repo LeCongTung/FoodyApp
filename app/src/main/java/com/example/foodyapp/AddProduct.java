7827e320d835844d0e3b600385f9a6ccfe094e4c
LeCongTung <tung.100301@gmail.com>
2021-12-09 23:43:28 +0700

    Intent AddProduct - ShowProduct

--- app/src/main/java/com/example/foodyapp/AddProduct.java
+++ app/src/main/java/com/example/foodyapp/AddProduct.java
@@ -4,19 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AddProduct extends AppCompatActivity {
+
+    ImageView btnback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
-//        Do from here
-
         ActivityCompat.requestPermissions(this, new String[]
                 {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+
+        btnback = (ImageView) findViewById(R.id.btnback);
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddProduct.this, ShowProduct.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/foodyapp/ShowProduct.java
+++ app/src/main/java/com/example/foodyapp/ShowProduct.java
@@ -1,14 +1,36 @@
 package com.example.foodyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ShowProduct extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    Button btnadd;
+    ImageView btnback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_product);
+
+        recyclerView = (RecyclerView) findViewById(R.id.listProduct);
+
+        btnback = (ImageView) findViewById(R.id.btnback);
+
+        btnadd = (Button) findViewById(R.id.btnaaddForm);
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ShowProduct.this, AddProduct.class);
+                startActivity(intent);
+            }
+        });
     }
 }
