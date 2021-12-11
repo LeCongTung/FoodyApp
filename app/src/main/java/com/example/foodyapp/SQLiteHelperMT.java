8a2fcc396388ae8431b2575e7af6e39254bfddca
LeCongTung <tung.100301@gmail.com>
2021-12-12 01:13:59 +0700

    Update Database

--- app/src/main/java/com/example/foodyapp/SQLiteHelperMT.java
+++ app/src/main/java/com/example/foodyapp/SQLiteHelperMT.java
@@ -2,8 +2,10 @@ package com.example.foodyapp;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -13,50 +15,44 @@ import java.io.FileNotFoundException;
 import java.sql.Blob;
 
 public class SQLiteHelperMT extends SQLiteOpenHelper {
+    //Init SQLite
+    private SQLiteDatabase db;
+    private SQLiteStatement st;
 
-    private Context context;
-    private static final String DATABASE_NAME = "Product.db";
-    private static final int DATABASE_VERSION = 1;
+    public SQLiteHelperMT(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
 
-    private static final String TABLE_NAME = "ProductDetailMT";
-    private static final String COLUMN_ID = "idProductMT";
-    private static final String COLUMN_NAME = "nameProductMT";
-    private static final String COLUMN_COST = "costProductMT";
-    private static final String COLUMN_LOCATION = "rateProductMT";
-    private static final String COLUMN_IMG = "imgProductMT";
+    public void queryData(String sql){
+        db = getWritableDatabase();
+        db.execSQL(sql);
+    }
 
+    public void addProductMT(String name, String price, String location, byte[] image){
+        db = getWritableDatabase();
+        String sql = "INSERT INTO TRASUA VALUES (NULL, ?, ?, ?)";
 
-    public SQLiteHelperMT(@Nullable Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-        this.context = context;
+        st = db.compileStatement(sql);
+        st.clearBindings();
+
+        st.bindString(1, name);
+        st.bindString(2, price);
+        st.bindString(3, location);
+        st.bindBlob(4, image);
+    }
+
+    public Cursor getData(String sql){
+        db = getReadableDatabase();
+        return db.rawQuery(sql, null);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase DB) {
-        String query = "CREATE TABLE " + TABLE_NAME +
-                "(" + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_NAME + "TEXT" + COLUMN_COST + "INTEGER"+ COLUMN_LOCATION + "TEXT" + COLUMN_IMG +"BLOB);";
-        DB.execSQL(query);
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
-        DB.execSQL("DROP TABLE IF EXISTS " +  TABLE_NAME);
-        onCreate(DB);
-    }
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
-    public long addProductMT(String name, String cost, String location, String image)  {
-        SQLiteDatabase DB = this.getWritableDatabase();
-
-        ContentValues CV = new ContentValues();;
-
-        CV.put(COLUMN_NAME, name);
-        CV.put(COLUMN_COST, cost);
-        CV.put(COLUMN_LOCATION, location);
-        CV.put(COLUMN_IMG, image);
-
-        long result = DB.insert(TABLE_NAME, null, CV);
-        DB.close();
-
-        return result;
     }
 }
