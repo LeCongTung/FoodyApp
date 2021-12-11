6461119117eb2c87aa7ee53e041b076f45bded75
LeCongTung <tung.100301@gmail.com>
2021-12-12 01:18:20 +0700

    Update Database

--- app/src/main/java/com/example/foodyapp/AddProduct.java
+++ app/src/main/java/com/example/foodyapp/AddProduct.java
@@ -1,9 +1,5 @@
 package com.example.foodyapp;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
@@ -12,12 +8,10 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -35,16 +29,8 @@ public class AddProduct extends AppCompatActivity {
    //view
     private ImageView btnback, showImg;
     private EditText inputName, inputCost, inputLocation;
-    private Button btnaddImage, btnaddProduct;
+    private Button btnaddImg, btnaddProduct;
     private SQLiteHelperMT dbMT;
-
-    //permission constants
-    private static final int CAMERA_REQUEST_CODE = 100;
-    private static final int STORAGE_REQUEST_CODE = 101;
-
-    //pick images from device
-    private static final int IMAGE_PICK_CAMERA_CODE = 102;
-    private static final int IMAGE_PICK_GALLERY_CODE = 102;
 
     //permission arrays
     private String[] cameraPermission;
@@ -69,16 +55,16 @@ public class AddProduct extends AppCompatActivity {
         inputLocation = (EditText) findViewById(R.id.locationP);
         showImg = (ImageView) findViewById(R.id.imgProduct);
 
-        sqlite = new SQLiteHelperMT(this);
 
         //Event: Click btnaddImage to adds an image
-        btnaddImage = (Button) findViewById(R.id.btnaddImage);
-        btnaddImage.setOnClickListener(new View.OnClickListener() {
+        btnaddImg = (Button) findViewById(R.id.btnaddImage);
+        btnaddImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                imagePickDialog();
+
             }
+
         });
 
         //Event: Click btnaddProduct to add an item
@@ -87,7 +73,7 @@ public class AddProduct extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                inputData();
+
             }
         });
 
@@ -104,42 +90,21 @@ public class AddProduct extends AppCompatActivity {
 
 
     //Event class
-        //Show dialog selection
-    private void imagePickDialog() {
-        String[] options = {"Camera", "Gallery"};
-        //Set AlertDialog
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Pick Image From");
-        builder.setItems(options, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                if (i == 0){
-                    if (!checkCameraPermission()){
-                        requestCameraPermission();
-                    }else{
-                        pickFromCamera();
-                    }
-                }else if (i==1){
-                    if (!checkStoragePermission()){
-                        requestStoragePermission();
-                    }else{
-                        pickFromGallery();
-                    }
-                }
-            }
-        });
-        builder.create().show();
-    }
-
     private void pickFromGallery() {
         Intent galleryIntent = new Intent(Intent.ACTION_PICK);
         galleryIntent.setType("image/*");
-        galleryActivities.launch(galleryIntent);
+        startActivityForResult(galleryIntent, IMAGE_PICK_GALLERY_CODE);
     }
 
     private void pickFromCamera() {
+        ContentValues CV = new ContentValues();
+        CV.put(MediaStore.Images.Media.TITLE, "Image Title");
+        CV.put(MediaStore.Images.Media.DESCRIPTION, "Image Description");
+        imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, CV);
+
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        cameraActivities.launch(cameraIntent);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(cameraIntent, IMAGE_PICK_CAMERA_CODE);
     }
 
     private void inputData() {
@@ -153,87 +118,5 @@ public class AddProduct extends AppCompatActivity {
         Toast.makeText(this, "Record Added with " + id, Toast.LENGTH_SHORT).show();
     }
 
-    //Set permission
-    private boolean checkStoragePermission(){
-        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
-        return result;
-    }
 
-    private void requestStoragePermission(){
-        ActivityCompat.requestPermissions(this, storagePermission, STORAGE_REQUEST_CODE);
-    }
-
-    private boolean checkCameraPermission(){
-        boolean resultC = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
-        boolean resultW = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
-        return resultC && resultW;
-    }
-
-    private void requestCameraPermission(){
-        ActivityCompat.requestPermissions(this, cameraPermission, CAMERA_REQUEST_CODE);
-    }
-
-    //Special activities
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        switch (requestCode){
-            case CAMERA_REQUEST_CODE:{
-                if (grantResults.length>0){
-                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    boolean storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-                    if (cameraAccepted && storageAccepted){
-                        pickFromCamera();
-                    }else{
-                        Toast.makeText(this, "Camera & Storage permissions are required!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-
-            }break;
-
-            case STORAGE_REQUEST_CODE:{
-                if (grantResults.length>0){
-                    boolean storageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-                    if (storageAccepted){
-                        pickFromCamera();
-                    }else{
-                        Toast.makeText(this, "Storage permissions are required!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }break;
-        }
-    }
-
-    private ActivityResultLauncher<Intent> galleryActivities = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == Activity.RESULT_OK){
-                        Intent data = result.getData();
-                        Uri imageURI = data.getData();
-                        showImg.setImageURI(imageURI);
-                    }else{
-                        Toast.makeText(AddProduct.this, "Cancelled!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-    );
-
-    private ActivityResultLauncher<Intent> cameraActivities = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == RESULT_OK && result.getData() != null){
-                        Bundle bundle = result.getData().getExtras();
-                        Bitmap bitmap = (Bitmap) bundle.get("data");
-                        showImg.setImageBitmap(bitmap);
-                    }else{
-                        Toast.makeText(AddProduct.this, "Cancelled!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-    );
 }
