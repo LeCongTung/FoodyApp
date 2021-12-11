0f98d8de8377a798d264823392b4b382d7d1cc3c
LeCongTung <tung.100301@gmail.com>
2021-12-12 02:08:01 +0700

    Update Database

--- app/src/main/java/com/example/foodyapp/AddProduct.java
+++ app/src/main/java/com/example/foodyapp/AddProduct.java
@@ -2,6 +2,11 @@ package com.example.foodyapp;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -12,6 +17,9 @@ import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -28,7 +36,7 @@ public class AddProduct extends AppCompatActivity {
 
    //View
     private ImageView btnback, showImg;
-    private EditText inputName, inputCost, inputLocation;
+    private EditText inputName, inputPrice, inputLocation;
     private Button btnaddProduct, btnaddImage;
     public static SQLiteHelperMT dbMT;
 
@@ -37,7 +45,7 @@ public class AddProduct extends AppCompatActivity {
     private String name, cost, location;
 
     //Permission
-
+    final int REQUEST_CODE_GALLERY = 999;
 
 
     @Override
@@ -52,7 +60,7 @@ public class AddProduct extends AppCompatActivity {
 
         //Init View
         inputName = (EditText) findViewById(R.id.nameP);
-        inputCost = (EditText) findViewById(R.id.costP);
+        inputPrice = (EditText) findViewById(R.id.costP);
         inputLocation = (EditText) findViewById(R.id.locationP);
         showImg = (ImageView) findViewById(R.id.imgProduct);
 
@@ -61,8 +69,10 @@ public class AddProduct extends AppCompatActivity {
         btnaddImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                ActivityCompat.requestPermissions(
+                        AddProduct.this,
+                        new String[] {Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY);
             }
 
         });
@@ -72,7 +82,21 @@ public class AddProduct extends AppCompatActivity {
         btnaddProduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                try {
+                    dbMT.addProductMT(
+                            inputName.getText().toString().trim(),
+                            inputPrice.getText().toString().trim(),
+                            inputLocation.getText().toString().trim(),
+                            imageViewToByte(showImg)
+                    );
+                    Toast.makeText(getApplicationContext(), "Added Successfully", Toast.LENGTH_SHORT).show();
+                    inputName.setText("");
+                    inputPrice.setText("");
+                    inputLocation.setText("");
+                    showImg.setImageResource(R.drawable.addimage);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
 
             }
         });
@@ -84,14 +108,51 @@ public class AddProduct extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(AddProduct.this, ShowProduct.class);
                 startActivity(intent);
+
             }
         });
 
 
     }
 
+    //Get images from Gallery
+    private byte[] imageViewToByte(ImageView image) {
+        Bitmap bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        return byteArray;
+    }
 
-    //Event class
 
+    //Declaration Permissions
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_GALLERY){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_GALLERY);
+            }else {
+                Toast.makeText(getApplicationContext(), "Error!", Toast.LENGTH_SHORT).show();
+            }
+        }
 
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == REQUEST_CODE_GALLERY && requestCode == RESULT_OK && data != null){
+            Uri imageURI = data.getData();
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(imageURI);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                showImg.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
