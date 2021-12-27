cee6b662ea1123635acd7f633036d94f23a3574e
LeCongTung <tung.100301@gmail.com>
2021-12-27 18:07:20 +0700

    Update carousel

--- app/src/main/java/com/example/foodyapp/Layout_Home.java
+++ app/src/main/java/com/example/foodyapp/Layout_Home.java
@@ -1,14 +1,22 @@
 package com.example.foodyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.helper.widget.Carousel;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Layout_Home extends AppCompatActivity {
+
+    private int[] slideAdver = new int[]{
+            R.drawable.panel1, R.drawable.panel2, R.drawable.panel3, R.drawable.panel4};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_home);
+
     }
+
+
 }
