00cbf5cbbcd6dcca496531fcd7b9e6db3e418456
LeCongTung <tung.100301@gmail.com>
2021-12-29 15:17:28 +0700

    Update Profile

--- app/src/main/java/com/example/foodyapp/Layout_Profile.java
+++ app/src/main/java/com/example/foodyapp/Layout_Profile.java
@@ -6,7 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.foodyapp.show.Show_AddProduct;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Layout_Profile extends AppCompatActivity {
@@ -15,6 +18,16 @@ public class Layout_Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_profile);
+
+        Button btntoshop = (Button) findViewById(R.id.shop);
+        btntoshop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Layout_Profile.this, Show_AddProduct.class));
+            }
+        });
+
+
 
         //Bottom navigation
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnav);
---
+++ app/src/main/java/com/example/foodyapp/show/Show_ListType.java
@@ -0,0 +1,16 @@
+package com.example.foodyapp.show;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.foodyapp.R;
+
+public class Show_ListType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_list_type);
+    }
+}
