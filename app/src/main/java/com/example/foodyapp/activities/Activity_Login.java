5591befb6a4503daaca4b12df5d7f8114a201f9c
khoadang1404 <khoadang.1404@gmail.com>
2022-01-04 09:58:47 +0700

    register form & login form p1

--- app/src/main/java/com/example/foodyapp/activities/Activity_Login.java
+++ app/src/main/java/com/example/foodyapp/activities/Activity_Login.java
@@ -6,9 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.foodyapp.Layout_Home;
 import com.example.foodyapp.R;
+import com.example.foodyapp.Register_form;
 
 public class Activity_Login extends AppCompatActivity {
 
@@ -17,11 +20,22 @@ public class Activity_Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        TextView tvregister = (TextView) findViewById(R.id.tvregister);
+
         Button btnlogin = (Button) findViewById(R.id.btnlogin);
         btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Activity_Login.this, Layout_Home.class);
+                startActivity(intent);
+                Toast.makeText(Activity_Login.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tvregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity_Login.this, Register_form.class);
                 startActivity(intent);
             }
         });
