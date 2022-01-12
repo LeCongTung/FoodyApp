842bbc481d69bf27cba9ba709aa93ebfca1c7317
LeCongTung <tung.100301@gmail.com>
2022-01-12 12:32:51 +0700

    Finish

--- app/src/main/java/com/example/foodyapp/Layout_Home.java
+++ app/src/main/java/com/example/foodyapp/Layout_Home.java
@@ -5,33 +5,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.foodyapp.adapters.Adapter_Discount;
+import com.example.foodyapp.adapters.Adapter_ListType;
 import com.example.foodyapp.adapters.Adapter_RecycleView;
 import com.example.foodyapp.adapters.Adaptersliderview;
 import com.example.foodyapp.show.Show_ListType;
 import com.example.foodyapp.show.Show_Search;
+import com.example.foodyapp.units.product;
 import com.example.foodyapp.units.voucher;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.smarteist.autoimageslider.SliderView;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Layout_Home extends AppCompatActivity {
 
+    ListView lv;
+    ArrayList<product> milkteaArray;
+    Adapter_Discount milkteaAdapter;
     public static Database db;
+    int numberorder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_home);
+
+        lv = (ListView) findViewById(R.id.list_item);
+        milkteaArray = new ArrayList<product>();
+        milkteaAdapter = new Adapter_Discount(this, R.layout.interface_item_indiscount, milkteaArray);
+        lv.setAdapter(milkteaAdapter);
 
         db = new Database(this, "Product.sqlite", null, 1);
         db.QueryData("CREATE TABLE IF NOT EXISTS cart(idC INTEGER PRIMARY KEY AUTOINCREMENT, nameC VARCHAR(255), priceC INTEGER, locationC VARCHAR(255), quantityC INTEGER, imageC BLOB)");
@@ -202,5 +226,138 @@ public class Layout_Home extends AppCompatActivity {
 
         listvoucher.setAdapter(adaptor);
 
+        showData();
     }
+
+    private void showData() {
+        Cursor cursor = db.GetData("SELECT * FROM product WHERE priceP < 35000");
+        milkteaArray.clear();
+        while (cursor.moveToNext()) {
+            milkteaArray.add(new
+                    product(cursor.getInt(0),
+                    cursor.getString(1),
+                    cursor.getInt(2),
+                    cursor.getString(3),
+                    cursor.getString(4),
+                    cursor.getString(5),
+                    cursor.getBlob(6)));
+        }
+        milkteaAdapter.notifyDataSetChanged();
+    }
+
+    public void DialogPay(String name, int price , byte[] image, String location, String description, final int id){
+
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog_cart_detail);
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        //dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().setAttributes(lp);
+
+        numberorder = 1;
+        Intent intent = getIntent();
+        String info = intent.getExtras().getString("info");
+        int total = intent.getIntExtra("total", 0);
+
+        //Select a position for dialog
+        Window window = dialog.getWindow();
+        WindowManager.LayoutParams param = window.getAttributes();
+        param.width = WindowManager.LayoutParams.MATCH_PARENT;
+        param.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        window.setGravity(Gravity.BOTTOM);
+        dialog.setContentView(R.layout.dialog_cart_detail);
+        dialog.setCancelable(true);
+
+
+        //Init elements
+        TextView etName = (TextView) dialog.findViewById(R.id.name);
+        TextView etQuantity = (TextView) dialog.findViewById(R.id.quantity);
+        TextView etTotal = (TextView) dialog.findViewById(R.id.total);
+        TextView etDescription = (TextView) dialog.findViewById(R.id.detail);
+        TextView etLocation = (TextView) dialog.findViewById(R.id.location);
+
+        Button btnplus = (Button) dialog.findViewById(R.id.btnplus);
+        Button btnminus = (Button) dialog.findViewById(R.id.btnminus);
+
+        ImageView imageView = (ImageView) dialog.findViewById(R.id.showImage);
+        Bitmap bm = BitmapFactory.decodeByteArray(image, 0, image.length);
+
+        //Show datas before add to cart
+        etName.setText(name);
+        etQuantity.setText("1");
+        int perProduct = price-5000;
+        etTotal.setText("" + perProduct);
+
+
+        imageView.setImageBitmap(bm);
+
+        etDescription.setText(description);
+        etLocation.setText("Địa chỉ: "  + location);
+
+        //Event: Plus one more product
+        btnplus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numberorder++;
+                etQuantity.setText(String.valueOf(numberorder));
+                int total = Integer.parseInt(etTotal.getText().toString().trim());
+                total += perProduct;
+                etTotal.setText(""+ total);
+                db.QueryData("UPDATE cart SET quantityC = quantityC + 1 WHERE idC ='" + id + "'");
+
+            }
+        });
+
+        //Event: Minus one less product
+        btnminus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (numberorder == 1){
+                    etTotal.setText(String.valueOf(perProduct));
+                    Toast.makeText(Layout_Home.this, "Đã đạt mức tối thiểu", Toast.LENGTH_SHORT).show();
+                }else{
+                    numberorder--;
+                    etQuantity.setText(String.valueOf(numberorder));
+                    int total = Integer.parseInt(etTotal.getText().toString().trim());
+                    total-= perProduct;
+                    etTotal.setText(""+ total);
+                    db.QueryData("UPDATE cart SET quantityC = quantityC - 1 WHERE idC ='" + id + "'");
+                }
+            }
+        });
+
+        Button btnaddcart = (Button) dialog.findViewById(R.id.btnaddcart);
+        btnaddcart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BitmapDrawable bm = (BitmapDrawable) imageView.getDrawable();
+                Bitmap bitmap = bm.getBitmap();
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+                byte[] image = byteArrayOutputStream.toByteArray();
+
+                int cost = Integer.parseInt(etTotal.getText().toString().trim());
+
+                Show_ListType.db.addC(
+                        etName.getText().toString().trim(),
+                        cost,
+                        etLocation.getText().toString().trim(),
+                        Integer.parseInt(etQuantity.getText().toString().trim()),
+                        image);
+
+                Intent intent = new Intent(Layout_Home.this, Layout_Cart.class);
+                intent.putExtra("info", info);
+                intent.putExtra("total", total + cost);
+                startActivity(intent);
+                overridePendingTransition(R.anim.anim_enter, R.anim.anim_enter_2);
+
+                dialog.dismiss();
+                Toast.makeText(Layout_Home.this, "Đã thêm vào giỏ hàng", Toast.LENGTH_SHORT).show();
+                showData();
+            }
+        });
+        dialog.show();
+    }
+
 }
---
+++ app/src/main/java/com/example/foodyapp/adapters/Adapter_Discount.java
@@ -0,0 +1,97 @@
+package com.example.foodyapp.adapters;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.foodyapp.Layout_Home;
+import com.example.foodyapp.R;
+import com.example.foodyapp.show.Show_ListType;
+import com.example.foodyapp.units.product;
+
+import java.util.List;
+
+public class Adapter_Discount extends BaseAdapter {
+    private Layout_Home context;
+    private int layout;
+    private List<product> milkteaList;
+
+    public Adapter_Discount(Context context, int layout, List<product> milkteaList) {
+        this.context = (Layout_Home) context;
+        this.layout = layout;
+        this.milkteaList = milkteaList;
+    }
+
+    @Override
+    public int getCount() {
+        return milkteaList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    private class ViewHolder{
+        TextView tvname, tvprice, tvtype, tvlocation, tvrate;
+        ImageView imageitem;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder vh;
+        if (view == null){
+            vh = new ViewHolder();
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(layout, null);
+            vh.tvname = (TextView) view.findViewById(R.id.name);
+            vh.tvprice = (TextView) view.findViewById(R.id.price);
+            vh.tvtype = (TextView) view.findViewById(R.id.type);
+            vh.tvlocation = (TextView) view.findViewById(R.id.description);
+            vh.tvrate = (TextView) view.findViewById(R.id.rate);
+
+            vh.imageitem = (ImageView) view.findViewById(R.id.image);
+
+            view.setTag(vh);
+        }else {
+            vh = (ViewHolder) view.getTag();
+        }
+
+        //Set random number for rate
+        double rand = 7 + (Math.random() * 3);
+        double finalnumberrate = Math.round(rand * 10.0)/10.0;
+
+        //Show datas
+        product mt = milkteaList.get(i);
+        vh.tvname.setText(mt.getName());
+        vh.tvprice.setText("Giá: " + String.valueOf(mt.getPrice()-5000) + " VNĐ");
+        vh.tvtype.setText(mt.getType());
+        vh.tvlocation.setText(mt.getLocation());
+        vh.tvrate.setText(""+ finalnumberrate);
+
+        byte[] imageM = mt.getImage();
+        Bitmap bm = BitmapFactory.decodeByteArray(imageM, 0, imageM.length);
+        vh.imageitem.setImageBitmap(bm);
+
+        //Event: Add to cart with item selected
+        vh.imageitem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context.DialogPay(mt.getName(), mt.getPrice(), mt.getImage(), mt.getLocation(), mt.getDescription(), mt.getId());
+            }
+        });
+
+        return view;
+   }
+}
